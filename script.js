0b866532a29b30d2425c0e38fbe087cb842a1556
buteryy <139429329+buteryy@users.noreply.github.com>
2025-02-18 23:37:25 -0800

    e

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const carouselInner = document.querySelector(".carousel-inner")
   const carouselItems = document.querySelectorAll(".carousel-item")
   const totalItems = carouselItems.length
-
   let index = 0 // carousel index
+
+  document.querySelector('form').addEventListener('submit', function (e) {
+    const form = e.target;
+  
+    setTimeout(() => {
+      form.reset(); // Clear the form fields after submission.
+    }, 100); // Delay to ensure submission completes before clearing.
+  });
+
+  // scrolling the page to the top after clicking on go back from formspree
+  window.addEventListener('popstate', function () {
+    window.scrollTo(0, 0); // Scroll to the top of the page when navigating back.
+  });
+  
 
   // Dynamically calculate the number of items visible at once
   const getVisibleItemsCount = () => {
